ef8ede4056306164797ced68074f99156b985406
hannahli11 <hannahli16492@gmail.com>
2024-12-10 14:58:38 -0800

    api/hannah

---
+++ app.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask
+
+# add an api endpoint to flask app
+@app.route('/api/hannah')
+def get_hannah_data():
+    InfoDb = []
+    InfoDb.append({
+        "FirstName": "Hannah",
+        "LastName": "Li",
+        "Username": "Hannahli_11"
+    })
+ # Return the list as JSON response
+    return jsonify(InfoDb)
+    
+
+# add an HTML endpoint to flask app
+@app.route('/')
+def say_hello():
+    html_content = """
+    <html>
+    <head>
+        <title>Hellox</title>
+    </head>
+    <body>
+        <h2>Hello, World!</h2>
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == '__main__':
+    # starts flask server on default port, http://127.0.0.1:5001
+    app.run(port=5001)

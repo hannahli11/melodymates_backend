b1c4eac661e6a63d2f76766934a639a13c4045ec
BrandonSmurlo <brandonsmurlo11@gmail.com>
2024-12-11 14:11:32 -0800

    user data file

---
+++ user_data.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask

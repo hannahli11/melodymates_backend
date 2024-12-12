338c25b228424b6ca3f9649fe874e5b50c8e83e8
BrandonSmurlo <brandonsmurlo11@gmail.com>
2024-12-12 13:52:16 -0800

    userdata api

--- api/userdata.py
+++ api/userdata.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api, Resource # used for REST API building
 
-student_api = Blueprint('student_api', __name__,
+userdata_api = Blueprint('userdata_api', __name__,
                    url_prefix='/api')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/
-api = Api(student_api)
+api = Api(userdata_api)
 
-class StudentAPI:        
+class userdataAPI:        
     class _John(Resource): 
         def get(self):
            # implement the get method 

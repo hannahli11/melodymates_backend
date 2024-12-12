482fd2772b58bac7d5feb691d69b5f311d9ba60f
BrandonSmurlo <brandonsmurlo11@gmail.com>
2024-12-12 14:13:31 -0800

    userdata updates

--- api/userdata.py
+++ api/userdata.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from flask_restful import Api, Resource # used for REST API building
+from flask import Flask, jsonify, Blueprint
+from flask_restful import Api
 
 userdata_api = Blueprint('userdata_api', __name__,
                    url_prefix='/api')
@@ -7,17 +7,60 @@ userdata_api = Blueprint('userdata_api', __name__,
 # API docs https://flask-restful.readthedocs.io/en/latest/
 api = Api(userdata_api)
 
-class userdataAPI:        
-    class _John(Resource): 
-        def get(self):
-           # implement the get method 
-           pass
-    
-    class _Jeff(Resource): 
-        def get(self):
-           # implement the get method 
-           pass
+app = Flask(__name__)
 
-    # building RESTapi endpoint
-    api.add_resource(_John, '/student/john')          
-    api.add_resource(_Jeff, '/student/jeff')
+# Simulated database for user information
+InfoDb = {
+    "Hannah": {
+        "FirstName": "Hannah",
+        "LastName": "Li",
+        "Username": "Hannahli_11"
+    },
+    "Carson": {
+        "FirstName": "Carson",
+        "LastName": "Sutherland",
+        "Username": "CJSuth$"
+    },
+    "Rhea": {
+        "FirstName": "Rhea",
+        "LastName": "Rajashekhar",
+        "Username": "rhear$"
+    },
+    "Brandon": {
+        "FirstName": "Brandon",
+        "LastName": "Smurlo",
+        "Username": "bsmurlo"
+    },
+    "Rowan": {
+        "FirstName": "Rowan",
+        "LastName": "Sutherland",
+        "Username": "rowangs.1"
+    }
+}
+
+@app.route('/<string:user_name>')
+def get_user_data(user_name):
+    # Look up the user by name in the InfoDb
+    user_data = InfoDb.get(user_name)
+    if user_data:
+        return jsonify(user_data)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/')
+def say_hello():
+    html_content = """
+    <html>
+    <head>
+        <title>Hello</title>
+    </head>
+    <body>
+        <h2>Welcome to the User Data API</h2>
+        <p>To access user data, append /[user_name] to the URL (e.g., /Hannah, /Carson, etc.).</p>
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == '__main__':
+    app.run(debug=True)

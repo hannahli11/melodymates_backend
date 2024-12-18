3edede3428b17ec56ab53facc6f693328cbece0c
hannahli11 <hannahli16492@gmail.com>
2024-12-17 16:50:11 -0800

    artist reccomendation  api

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ from api.messages_api import messages_api # Adi added this, messages for his web
 from api.carphoto import car_api
 from api.carChat import car_chat_api
 from api.student import student_api
+from api.artrec import artrec_api
 
 from api.vote import vote_api
 # database Initialization functions
@@ -57,6 +58,7 @@ app.register_blueprint(nestImg_api)
 app.register_blueprint(vote_api)
 app.register_blueprint(car_api)
 app.register_blueprint(student_api)
+app.register_blueprint(artrec_api)
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"

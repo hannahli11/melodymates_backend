57a1a54cbf759668143f63c34ae8d3f5aa831f91
rowangs <rowan.sutherland@icloud.com>
2024-12-18 00:48:02 -0800

    duplicate

--- api/musicChat.py
+++ api/musicChat.py
@@ -104,12 +104,6 @@ if __name__ == '__main__':
     # Start Flask server on http://127.0.0.1:5001
     app.run(port=5001)
 
-musicChat_api = Blueprint('musicChat_api', __name__, url_prefix='/api')
-app = Flask(__name__)
-CORS(app, supports_credentials=True, origins='*')  
-# API docs https://flask-restful.readthedocs.io/en/latest/
-api = Api(musicChat_api)
-
 
 class musicChatAPI:
    # @app.route('/api/student/')

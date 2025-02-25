be9148dcf0b3a6ed6454189a66cf70312a57b749
hannahli11 <hannahli16492@gmail.com>
2025-02-25 14:16:35 -0800

    e

--- api/artrec.py
+++ api/artrec.py
@@ -6,7 +6,7 @@ from model.artInfo import ArtInfo
 
 artrec_api = Blueprint('artrec_api', __name__, url_prefix='/api') 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:8404"]}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:8404", "https://melodymates.stu.nighthawkcodingsociety.com"]}})
 # API docs https://flask-restful.readthedocs.io/en/latest/
 api = Api(artrec_api)
 
@@ -234,6 +234,7 @@ class ArtInfoResource(Resource):
 
       
 # Building REST API endpoint
+api.add_resource(ArtInfoResource, '/artinfo')
 '''
 api.add_resource(HannahResource, '/user/Hannah')
 api.add_resource(RheaResource, '/user/Rhea')
@@ -242,7 +243,7 @@ api.add_resource(RowanResource, '/user/Rowan')
 api.add_resource(CarsonResource, '/user/Carson')
 api.add_resource(BrandonResource, '/user/Brandon')'
 '''
-api.add_resource(ArtInfoResource, '/artinfo')  # POST, GET, PUT, DELETE for /api/artinfo
+  # POST, GET, PUT, DELETE for /api/artinfo
 
 
 
